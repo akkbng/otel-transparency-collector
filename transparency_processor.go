109eb982725901fdf9116891a55f618a5474590b
Bengisu Akkir <bengisuakkir@gmail.com>
2023-06-26 21:42:54 +0200

    try out node graph api

--- transparency_processor.go
+++ transparency_processor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspan"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/pdata/pcommon"
+	"net/http"
 	"sync"
 	"time"
 
@@ -59,6 +60,7 @@ func newTransparencyProcessor(logger *zap.Logger, skipExpr expr.BoolExpr[ottlspa
 }
 
 func (a *transparencyProcessor) processTraces(ctx context.Context, td ptrace.Traces) (ptrace.Traces, error) {
+	server()
 	rss := td.ResourceSpans()
 	for i := 0; i < rss.Len(); i++ {
 		rs := rss.At(i)
@@ -97,4 +99,103 @@ func insertTiltCheck(span ptrace.Span, tiltComponent pcommon.Value) {
 	} else {
 		span.Attributes().PutBool(attrCheckFlag, false)
 	}
+}
+
+func server() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/graph/data", getGraph)
+	mux.HandleFunc("/api/graph/fields", getFields)
+
+	http.ListenAndServe(":4318", mux)
+}
+
+// returns graph fields as json object, and in application/json format
+func getFields(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write([]byte(`{
+  "edges_fields": [
+    {
+      "field_name": "id",
+      "type": "string"
+    },
+    {
+      "field_name": "source",
+      "type": "string"
+    },
+    {
+      "field_name": "target",
+      "type": "string"
+    },
+    {
+      "field_name": "mainStat",
+      "type": "number"
+    }
+  ],
+  "nodes_fields": [
+    {
+      "field_name": "id",
+      "type": "string"
+    },
+    {
+      "field_name": "title",
+      "type": "string"
+    },
+    {
+      "field_name": "mainStat",
+      "type": "string"
+    },
+    {
+      "field_name": "secondaryStat",
+      "type": "number"
+    },
+    {
+      "color": "red",
+      "field_name": "arc__failed",
+      "type": "number"
+    },
+    {
+      "color": "green",
+      "field_name": "arc__passed",
+      "type": "number"
+    },
+    {
+      "displayName": "Role",
+      "field_name": "detail__role",
+      "type": "string"
+    }
+  ]
+}`))
+}
+
+// returns graph data as json object, and in application/json format
+func getGraph(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write([]byte(`{
+    "edges": [
+        {
+            "id": "1",
+            "mainStat": "53/s",
+            "source": "1",
+            "target": "2"
+        }
+    ],
+    "nodes": [
+        {
+            "arc__failed": 0.7,
+            "arc__passed": 0.3,
+            "detail__zone": "load",
+            "id": "1",
+            "subTitle": "instance:#2",
+            "title": "Service1"
+        },
+        {
+            "arc__failed": 0.5,
+            "arc__passed": 0.5,
+            "detail__zone": "transform",
+            "id": "2",
+            "subTitle": "instance:#3",
+            "title": "Service2"
+        }
+    ]
+}`))
 }
