43689618110a4eb9b896b08418234df18918709f
Bengisu Akkir <bengisuakkir@gmail.com>
2023-06-30 16:55:54 +0200

    add tilt_check to every span, then make sampling decision

--- internal/sampling/transparency_tag_filter.go
+++ internal/sampling/transparency_tag_filter.go
@@ -2,6 +2,8 @@ package sampling
 
 import (
 	"context"
+	"github.com/akkbng/otel-transparency-collector/internal/filter/expr"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/ottl/contexts/ottlspan"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
@@ -34,22 +36,9 @@ type tiltAttributes struct {
 	serviceName         string
 }
 
-func insertTiltCheck(span ptrace.Span) bool {
-	if tiltComponent, ok := span.Attributes().Get(attrCategories); ok {
-		//if tiltComponent value is not empty, add "true" as the value of the checkFlag attribute
-		if tiltComponent.AsString() != "" {
-			span.Attributes().PutBool(attrCheckFlag, true)
-			return true
-		} else {
-			span.Attributes().PutBool(attrCheckFlag, false)
-			return false
-		}
-	}
-	return false
-}
-
 type transparencyAttributeFilter struct {
-	logger *zap.Logger
+	logger   *zap.Logger
+	skipExpr expr.BoolExpr[ottlspan.TransformContext]
 }
 
 var _ PolicyEvaluator = (*transparencyAttributeFilter)(nil)
@@ -61,12 +50,62 @@ func NewTransparencyAttributeFilter(settings component.TelemetrySettings) Policy
 }
 
 // Evaluate looks at the trace data and returns a corresponding SamplingDecision.
-func (taf *transparencyAttributeFilter) Evaluate(_ context.Context, _ pcommon.TraceID, trace *TraceData) (Decision, error) {
+func (taf *transparencyAttributeFilter) Evaluate(ctx context.Context, _ pcommon.TraceID, trace *TraceData) (Decision, error) {
 	trace.Lock()
 	batches := trace.ReceivedBatches
 	trace.Unlock()
 
-	return hasSpanWithCondition(batches, func(span ptrace.Span) bool {
-		return insertTiltCheck(span)
-	}), nil
+	for i := 0; i < batches.ResourceSpans().Len(); i++ {
+		rs := batches.ResourceSpans().At(i)
+		resource := rs.Resource()
+		for j := 0; j < rs.ScopeSpans().Len(); j++ {
+			ss := rs.ScopeSpans().At(j)
+			scope := ss.Scope()
+			for k := 0; k < ss.Spans().Len(); k++ {
+				span := ss.Spans().At(k)
+
+				var (
+					skip bool
+					err  error
+				)
+
+				// Now we reach span level and begin evaluation with parsed expr.
+				// The evaluation will break when:
+				// 1. error happened.
+				// 2. "Sampled" decision made.
+				// Otherwise, it will keep evaluating and finally exit with "NotSampled" decision.
+
+				// Span evaluation
+				if taf.skipExpr != nil {
+					skip, err = taf.skipExpr.Eval(ctx, ottlspan.NewTransformContext(span, scope, resource))
+					if err != nil {
+						return Error, err
+					}
+					if skip {
+						continue
+					}
+				}
+				tiltComponent, ok := span.Attributes().Get(attrCategories)
+				if !ok {
+					continue
+				}
+				insertTiltCheck(span, tiltComponent)
+				if insertTiltCheck(span, tiltComponent) == true {
+					return Sampled, nil
+				}
+			}
+		}
+	}
+	return NotSampled, nil
+}
+
+func insertTiltCheck(span ptrace.Span, tiltComponent pcommon.Value) bool {
+	//if tiltComponent value is not empty, add "true" as the value of the checkFlag attribute
+	if tiltComponent.AsString() != "" {
+		span.Attributes().PutBool(attrCheckFlag, true)
+		return true
+	} else {
+		span.Attributes().PutBool(attrCheckFlag, false)
+		return false
+	}
 }
